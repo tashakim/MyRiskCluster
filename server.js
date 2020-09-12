dc7d6ed71d29c23226b3f34a562696990267f97d
Tasha Kim <jae_young_kim@brown.edu>
2020-09-13 07:03:51 +0900

    Create instance of express app

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 var express = require("express");
+var logger = require("morgan");
 
 var app = express();
+
+// Create instance of express app
+app.use(logger("dev"));
+
+app.get("/", function (request, response) {
+  response.send("<h1> Prep for Prep</h1>");
+});
+
+var port = 8080;
+
+app.listen(port, function () {
+  console.log("App running on port " + port);
+});
