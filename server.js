06878658978d835e8eb9e544deb58851920f1a85
Tasha Kim <jae_young_kim@brown.edu>
2020-09-13 07:53:31 +0900

    Integrate firebase db

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 var express = require("express");
 var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 // Create instance of express app
 var app = express();
 
 // We will serve js and html in ejs
-app.set("view engine", ejs);
+app.set("view engine", "ejs");
 
+// Send css, images, other static files
+app.use(express.static("views"));
+
+// Give server access to the user input
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("views", __dirname + "/views");
 app.use(logger("dev"));
 
 app.get("/", function (request, response) {
-  response.send("<h1> Prep for Prep</h1>");
+  //response.send("<h1> Prep for Prep</h1>");
+  response.render("home.ejs");
+});
+
+app.post("/", function (request, response) {
+  // Send back a page with information on it
+  var region = request.body.region;
+  response.render("results.ejs", { data: region });
 });
 
 var port = 8080;
